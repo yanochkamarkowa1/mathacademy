dd3254102c19a4c4cd077a6ca89cfaa7516312c1
Ìàðêîâà ßíà <yanochka.markowa@yandex.ru>
2017-03-01 20:02:01 +0300

    Что-то сделала

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?
-echo ('Hello World');
+<html>
+<head>
+
+</head>
+<body>
+<h1>Стартовая страница</h1>
+<div>
+    <ul>
+        <?for ($i = 0; $i < 100; $i++):?>
+        <li>Пункт <?=$i?></li>
+        <?endfor;?>
+    </ul>
+</div>
+</body>
+</html>
